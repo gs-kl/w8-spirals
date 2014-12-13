a13cefed4f8ad5714e1e1245a920af5a7e37cddf
Kevin <litchfield.kevin+gs@gmail.com>
2014-12-13 00:29:12 -0800

    Prints first five numbers in correct spaces

--- lib/spirals.rb
+++ lib/spirals.rb
@@ -1,50 +1,99 @@
-class Spiral
-  def initialize start_number, width
-    @current_number = start_number
-    @width = width
-    @directions = ["R", "D", "L", "U"]
-    @facing = "R"
-    @cursor = [0,0]
-  end
+class Cursor
+  attr_accessor :column, :row
 
-  def create_grid 
-    @grid = Array.new
-
-    blank_line = Array.new
-    @width.times do 
-      blank_line.push(Array.new)
-    end
-
-    (1..@width).each do 
-      @grid.push(Array.new(@width){Array.new})
-      # @grid.push(blank_line)
-    end
-
-    @grid2 = [[[],[],[]],[[],[],[]],[[],[],[]]]
-  end
-
-  def write_num
-    p @grid
-    # @grid[@cursor[0]][@cursor[1]] << @current_number
-
-    @grid[0][0].push(1)
-    @current_number += 1
-
-    p @grid
-  end
-
-
-
-  def look_ahead
-  end
-
-  def set_direction
-  end
-
-  def move
+  def initialize
+    @column = 0
+    @row = 0
   end
 end
 
-s = Spiral.new(1,3)
-s.create_grid
-s.write_num
+class Spiral
+  attr_accessor :direction, :cursor, :grid
+
+  def initialize start_number, width
+    @current_number = start_number
+    @width = width
+    @direction = "Right"
+    @cursor = Cursor.new
+    @grid = create_grid
+  end
+
+  def create_grid
+    grid = Array.new
+    (1..@width).each do 
+      grid.push(Array.new(@width){Array.new})
+    end
+    grid
+  end
+
+  def write_num
+    @grid[cursor.row][cursor.column] << @current_number
+    @current_number += 1
+  end
+
+  def look_ahead
+    case @direction
+    when "Right"
+      if @grid[@cursor.row][@cursor.column + 1].nil?
+        rotate
+      elsif @cursor.column + 1 == @width
+        rotate
+      end
+    when "Down"
+      if @grid[@cursor.row + 1].nil?
+        rotate
+        print "ASLDKALSKD"
+      elsif @cursor.row + 1 == @width
+        rotate
+        print "QWEQWE"
+      end
+    when "Left"
+      rotate if @cursor.row - 1 < 0
+    when "Up"
+      rotate if @cursor.column - 1 < 0
+    end
+  end
+
+  def rotate
+    directions = ["Right", "Down", "Left", "Up"]
+    @direction = directions[(directions.index(@direction) + 1) % 4]
+  end
+
+  def move
+    case @direction
+    when "Right"
+      @cursor.column = @cursor.column.next
+    when "Down"
+      @cursor.row = @cursor.row.next
+    when "Left"
+      @cursor.column = @cursor.column.pred
+    when "Up"
+      @cursor.row = @cursor.row.pred
+    end
+  end
+
+  # def print
+  #   p @grid
+  # end
+
+  def run
+    self.write_num
+    self.look_ahead
+    self.move
+  end
+
+end
+
+
+
+
+spiral = Spiral.new(1,3)
+
+5.times do
+  p spiral.cursor.row
+  spiral.run
+  p spiral.direction
+  # spiral.print
+  p spiral.grid
+end
+
--- spec/spirals_spec.rb
+++ spec/spirals_spec.rb
@@ -1,6 +1,38 @@
+require_relative '../lib/spirals.rb'
+
+describe "Spiral class initialized with start number 1, width 3" do
+  it "creates the appropriate grid" do
+    spiral = Spiral.new 1,3
+    output = spiral.print.to_s
+    expect(output).to eq("[[[], [], []], [[], [], []], [[], [], []]]")
+  end
+
+  it "writes the first number" do
+    spiral = Spiral.new 1,3
+    spiral.write_num
+    output = spiral.print.to_s
+    expect(output).to eq("[[[1], [], []], [[], [], []], [[], [], []]]")
+  end
+
+  it "writes the first number, looks ahead, and does not rotate" do
+spiral = Spiral.new 1,3
+   spiral.write_num
+    spiral.look_ahead
+    output = spiral.direction
+    expect(output).to eq("Right")
+end
+
+  it "writes the first number, looks ahead, and does not rotate, and moves" do
+spiral = Spiral.new 1,3
+    spiral.write_num
+    spiral.look_ahead
+    spiral.move
+    output = spiral.cursor
+    expect(output).to eq(1)
+end
 
 
-
+end
 
 
 
